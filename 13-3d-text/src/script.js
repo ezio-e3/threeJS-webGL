37cc173469486edc82b99dd13fe849c626684adb
Emmanuel Kofi Mensah <edin22.ey@gmail.com>
2023-06-13 22:04:25 +0000

    3d-text-complete

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -16,11 +16,17 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xf9e0bb)
+
+//Axes Helper
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matCapTexture = textureLoader.load('/textures/matcaps/8.png')
 
 /**
  * Fonts
@@ -29,7 +35,7 @@ const fontLoader = new FontLoader()
 fontLoader.load('/fonts/helvetiker_bold.typeface.json', 
 (font) => {
     const textGeometry = new TextGeometry(
-        'Edinams Design Visualisation and Simulation Studio', 
+        'Three.js Donut Shop', 
         {
             font: font,
             size: 0.5,
@@ -43,7 +49,17 @@ fontLoader.load('/fonts/helvetiker_bold.typeface.json',
 
         }
     )
-    const textMaterial = new THREE.MeshBasicMaterial({wireframe: true})
+    textGeometry.computeBoundingBox()
+    // textGeometry.translate(
+    //    - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+    //    - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+    //    - (textGeometry.boundingBox.max.z - 0.03) * 0.5
+
+    // )
+    textGeometry.center()
+
+    const textMaterial = new THREE.MeshMatcapMaterial()
+    textMaterial.matcap = matCapTexture
     const text = new THREE.Mesh(textGeometry, textMaterial)
     scene.add(text)
 })
@@ -51,10 +67,26 @@ fontLoader.load('/fonts/helvetiker_bold.typeface.json',
 /**
  * Object
  */
-// const cube = new THREE.Mesh(
-//     new THREE.BoxGeometry(1, 1, 1),
-//     new THREE.MeshBasicMaterial()
-// )
+const torus = new THREE.TorusGeometry(0.3, 0.2 ,20 ,45)
+const torusMaterial = new THREE.MeshMatcapMaterial({matcap : matCapTexture})
+for (let i = 0; i < 280; i++){
+    const donut = new THREE.Mesh(torus, torusMaterial)
+    donut.position.x = (Math.random() - 0.5) * 10
+    donut.position.y = (Math.random() - 0.5) * 10
+    donut.position.z = (Math.random() - 0.5) * 10
+
+    donut.rotation.x = Math.random() * Math.PI
+    donut.rotation.y = Math.random() * Math.PI
+
+    const scale = Math.random()
+    donut.scale.x = scale
+    donut.scale.y = scale
+    donut.scale.z = scale
+
+
+
+    scene.add(donut)
+}
 
 //scene.add(cube)
 
